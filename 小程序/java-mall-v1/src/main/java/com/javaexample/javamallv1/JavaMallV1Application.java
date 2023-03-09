0cbf986773ef456993587c83c4c91736aed12b1e
aisaka0515 <60870401+aisaka0515@users.noreply.github.com>
2023-03-09 20:34:29 +0800

    Add files via upload

---
+++ 小程序/java-mall-v1/src/main/java/com/javaexample/javamallv1/JavaMallV1Application.java
@@ -0,0 +1,13 @@
+package com.javaexample.javamallv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMallV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMallV1Application.class, args);
+    }
+
+}
